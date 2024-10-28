bd70e97ce774dd25a5ca8297aa8370425aa42c19
MR211604 <elmastrollxd34@gmail.com>
2024-10-27 19:45:10 -0600

    Solventando seguridad

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBxwMAQaZ13syc8XpDsEM0D_CrveUK7xXg",
-  authDomain: "react-journal-app-8f0ed.firebaseapp.com",
-  projectId: "react-journal-app-8f0ed",
-  storageBucket: "react-journal-app-8f0ed.appspot.com",
-  messagingSenderId: "872742176428",
-  appId: "1:872742176428:web:704043b76a581590368109"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_FIREBASE_APP_ID
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
